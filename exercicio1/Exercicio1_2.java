f2ee4cbb7eed57938066d55b6e8e81655ece44d8
Lavinia Sayuri <lavinia_maria.vitor@hotmail.com>
2016-03-03 12:48:43 -0300

    Update Exercicio1_2.java

--- exercicio1/Exercicio1_2.java
+++ exercicio1/Exercicio1_2.java
@@ -1,1 +1,6 @@
-...
+  public class Exercicio1_2 {
+    public static void main(String [] args){
+      String ARGUMENTO = args[0];
+      System.out.println("O argumento digitado foi: " + ARGUMENTO);
+    }
+  }

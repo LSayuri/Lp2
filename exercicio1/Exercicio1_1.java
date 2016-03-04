59b47a02e24f6ba986e8f2fcf54a406ea3a10076
Lavinia Sayuri <lavinia_maria.vitor@hotmail.com>
2016-03-04 08:36:55 -0300

    Update Exercicio1_1.java

--- exercicio1/Exercicio1_1.java
+++ exercicio1/Exercicio1_1.java
@@ -1,6 +1,6 @@
   public class Exercicio1_1 {
-    public static void main(String [] cicla){
-      String CICLANO = cicla[0];
+    public static void main(String [] args){
+      String CICLANO = args[0];
       System.out.println("O meu nome eh " + CICLANO);
     }
   }

32b27786018ae7a1d89e9df6cf3b0cabf9a653d8
Lavinia Sayuri <lavinia_maria.vitor@hotmail.com>
2016-03-04 07:43:52 -0300

    Update Exponencial.java

--- exercicio2/Exponencial.java
+++ exercicio2/Exponencial.java
@@ -1,1 +1,9 @@
-...
+public class Exercicio1_5 {
+  public static void main(String [] args) {
+    System.out.println("#    Q    C");
+    int n = 0;
+    for (int n = 0; n<=10, n++){
+      System.out.println(n + "   " + n ^ 2 + "   " + n3);  
+    }
+  }
+}
